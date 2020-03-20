0977dfab220fae877985ecf14366d9550f70aa6c
Kitiyaparnnn <59277374+Kitiyaparnnn@users.noreply.github.com>
2020-03-20 15:44:14 +0700

    Update showcard n score.cpp

--- showcard n score.cpp
+++ showcard n score.cpp
@@ -1,6 +1,7 @@
 void Show(unit x){
 	cout<<"*************************";
-	cout<<""<<x.name<<"'s card is: "<<x.onhand<<"\n";
+	for(int i=0;i<x.onhand.size();i++)
+		cout<<""<<x.name<<"'s card is: "<<x.onhand[i]<<"\n";
 	cout<<""<<x.name<<"'s score is: "<<x.score<<"\n";
 	cout<<"*************************";
 }
