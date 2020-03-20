e59338a3f3779ecd6fa9b7b3f30d635e6cd1dfd5
Kitiyaparnnn <59277374+Kitiyaparnnn@users.noreply.github.com>
2020-03-20 15:40:40 +0700

    Add files via upload

---
+++ showcard n score.cpp
@@ -0,0 +1,6 @@
+void Show(unit x){
+	cout<<"*************************";
+	cout<<""<<x.name<<"'s card is: "<<x.onhand<<"\n";
+	cout<<""<<x.name<<"'s score is: "<<x.score<<"\n";
+	cout<<"*************************";
+}
