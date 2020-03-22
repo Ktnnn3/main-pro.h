c822c1901ff6712de8b1599bf504f4cdef595d80
Kitiyaparnnn <59277374+Kitiyaparnnn@users.noreply.github.com>
2020-03-22 22:12:53 +0700

    Add files via upload

---
+++ leaderB.cpp
@@ -0,0 +1,15 @@
+while(leaderB == 1){
+	cout<<"--------------------------------------------\n";
+	cout<<setw(30)<<"G A M E R A N K \n";
+	cout<<"      calculateed from your items.\n";
+	cout<<"--------------------------------------------\n";
+	cout<<setw(10)<<"Rank"<<setw(30)<<"Payment\n";	
+	cout<<"--------------------------------------------\n";
+	cout<<"[S] Pirate king"<<setw(30)<<"500+\n";
+	cout<<"[A] First mate"<<setw(31)<<"450-499\n";
+	cout<<"[B] Advisor"<<setw(34)<<"400-449\n";
+	cout<<"[C] Assassin"<<setw(33)<<"300-399\n";
+	cout<<"[D] Servant"<<setw(34)<<"200-299\n";
+	cout<<"[F] Cabin Boy"<<setw(32)<<"0-199\n";
+	cout<<"--------------------------------------------\n";
+}
