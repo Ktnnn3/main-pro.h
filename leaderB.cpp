da8fd10d15a470bdc8ccdf7d7ed3fd26be0af19c
Kitiyaparnnn <59277374+Kitiyaparnnn@users.noreply.github.com>
2020-04-10 15:07:00 +0700

    Update leaderB.cpp

--- leaderB.cpp
+++ leaderB.cpp
@@ -1,15 +1,15 @@
-while(leaderB == 1){
+void leaderfunc(){
 	cout<<"--------------------------------------------\n";
 	cout<<setw(30)<<"G A M E R A N K \n";
 	cout<<"      calculateed from your items.\n";
 	cout<<"--------------------------------------------\n";
 	cout<<setw(10)<<"Rank"<<setw(30)<<"Payment\n";	
 	cout<<"--------------------------------------------\n";
-	cout<<"[S] Pirate king"<<setw(30)<<"500+\n";
-	cout<<"[A] First mate"<<setw(31)<<"450-499\n";
-	cout<<"[B] Advisor"<<setw(34)<<"400-449\n";
-	cout<<"[C] Assassin"<<setw(33)<<"300-399\n";
-	cout<<"[D] Servant"<<setw(34)<<"200-299\n";
-	cout<<"[F] Cabin Boy"<<setw(32)<<"0-199\n";
+	cout<<"[S] Pirate king"<<setw(30)<<"12,000,000+\n";
+	cout<<"[A] First mate"<<setw(31)<<"1,000,000-11,999,999\n";
+	cout<<"[B] Advisor"<<setw(34)<<"500,000-999,999\n";
+	cout<<"[C] Assassin"<<setw(33)<<"10,001-499,999\n";
+	cout<<"[D] Servant"<<setw(34)<<"501-10,000\n";
+	cout<<"[F] Cabin Boy"<<setw(32)<<"0-500\n";
 	cout<<"--------------------------------------------\n";
 }
