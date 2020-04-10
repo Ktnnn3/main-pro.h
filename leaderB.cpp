7c728b313f5aec87d266bd0de8928c40d578e860
Kitiyaparnnn <59277374+Kitiyaparnnn@users.noreply.github.com>
2020-04-10 15:17:27 +0700

    Update leaderB.cpp

--- leaderB.cpp
+++ leaderB.cpp
@@ -5,8 +5,8 @@ void leaderfunc(){
 	cout<<"--------------------------------------------\n";
 	cout<<setw(10)<<"Rank"<<setw(30)<<"Payment\n";	
 	cout<<"--------------------------------------------\n";
-	cout<<"[S] Pirate king"<<setw(30)<<"12,000,000+\n";
-	cout<<"[A] First mate"<<setw(31)<<"1,000,000-11,999,999\n";
+	cout<<"[S] Pirate king"<<setw(30)<<"10,000,000+\n";
+	cout<<"[A] First mate"<<setw(31)<<"1,000,000-9,999,999\n";
 	cout<<"[B] Advisor"<<setw(34)<<"500,000-999,999\n";
 	cout<<"[C] Assassin"<<setw(33)<<"10,001-499,999\n";
 	cout<<"[D] Servant"<<setw(34)<<"501-10,000\n";
